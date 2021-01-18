964cd0872f3c49c995949cdebec60b3fede0e135
priyankasiddesh <priyasonu.278@gmail.com>
2021-01-18 15:24:44 +0530

    spring boot first commit

---
+++ src/main/java/io/priya/springbootstarter/hello/HelloController.java
@@ -0,0 +1,13 @@
+package io.priya.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String sayHai() {
+		return "Hi";
+	}
+
+}
---
+++ src/main/java/io/priya/springbootstarter/topic/TopicService.java
@@ -0,0 +1,45 @@
+package io.priya.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.*;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics= new ArrayList<>(Arrays.asList(
+			new Topic("spring","spring framework","spring framework description"),
+			new Topic("java","core java","java description"),
+			new Topic("javascript","react js","javascript description")
+			));
+public List<Topic> getAllTopics(){
+	return topics;
+}
+public Topic getTopic(String id) {
+	return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+}
+public void addTopic(Topic topic) {
+	// TODO Auto-generated method stub
+	topics.add(topic);
+	
+}
+public void updateTopic(Topic topic, String id) {
+	// TODO Auto-generated method stub
+	for(int i=0;i<topics.size();i++) {
+		Topic t=topics.get(i);
+		if(t.getId().equals(id)) {
+			topics.set(i, topic);
+			return;
+		}
+		
+	}
+	
+}
+public void deleteTopic(String id) {
+	// TODO Auto-generated method stub
+	topics.removeIf(t->t.getId().equals(id));
+}
+
+
+}
